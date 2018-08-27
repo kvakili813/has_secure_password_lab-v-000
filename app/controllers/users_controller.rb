13ab71edf97429173a78ec56b73d3db58ef5edda
Kayvan Vakili <kvakili813@gmail.com>
2018-08-27 19:37:50 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    binding.pry
     if params[:user][:password] == params[:user][:password_confirmation]
       @user = User.create(user_params)
       if @user

eb44579e22608d033f14cf18d906922f842a35bf
Kayvan Vakili <kvakili813@gmail.com>
2018-08-27 19:58:12 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20180827052145) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "password_digest"
+    t.string "password"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
